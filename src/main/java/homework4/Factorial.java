93225828bb9cb07f97c9e788e61f590330784c28
Gvantsa Kikvidze <gkikvidze@gmail.com>
2026-05-31 17:07:04 +0400

    [Gvantsa k.] HW4 — Factorial

---
+++ src/main/java/homework4/Factorial.java
@@ -0,0 +1,13 @@
+package homework4;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int birthMonth = 3;
+        long factorial = 1;
+        for (int i = 1; i <= birthMonth; i++) {
+            factorial *= i;
+        }
+        System.out.println(birthMonth + "! = " + factorial);
+
+    }
+}

7bfca29be14413c0805608422cb9bb650a420df2
Gvantsa Kikvidze <gkikvidze@gmail.com>
2026-05-31 14:37:44 +0400

    [gvanca k.] Initial Maven project

---
+++ src/main/java/ge/credo/App.java
@@ -0,0 +1,7 @@
+package ge.credo;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/ge/credo/AppTest.java
@@ -0,0 +1,13 @@
+package ge.credo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}

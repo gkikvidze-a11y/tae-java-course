759c0fd73941acec22f036b5d02fd9433754d4ce
Gvantsa Kikvidze <gkikvidze@gmail.com>
2026-06-07 22:01:16 +0400

    [Gvantsa k.] HW6 — Student

--- src/main/java/homework6/Animal.java
+++ src/main/java/homework6/Animal.java
@@ -1,4 +1,13 @@
 package homework6;
 
 public class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("some sound");
+    }
 }

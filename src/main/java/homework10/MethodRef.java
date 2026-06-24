b7e6ed628d3d20cdc8793fbe61a3088c502a8401
Gvantsa Kikvidze <gkikvidze@gmail.com>
2026-06-24 10:17:20 +0400

    [gvantsa k.] HW10 — MethodRef

---
+++ src/main/java/homework10/MethodRef.java
@@ -0,0 +1,24 @@
+
+package homework10;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MethodRef {
+    public static void main(String[] args) {
+        List<String> names = List.of("Anna", "Beka", "Cita");
+        List<String> stringNumbers = List.of("1", "2", "3");
+
+        names.forEach(System.out::println);
+
+        List<String> upper = names.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        System.out.println(upper);
+
+        List<Integer> ints = stringNumbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        System.out.println(ints);
+    }
+}
